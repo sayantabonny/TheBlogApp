78384428b12090c0e9773d2d0008891989a12336
Sayanta <sayantabonny@gmail.com>
2024-07-21 12:25:40 +0530

    Initial Commit

---
+++ src/main/java/com/sayantabonny/theblogapp/TheBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.sayantabonny.theblogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBlogAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheBlogAppApplication.class, args);
+    }
+
+}
